67d016982a72b6c8b82798a01eef4d968e3a98fe
Tejahaha <2300033461@kluniversity.in>
2025-05-11 07:34:04 +0530

    hi

--- src/main/java/com/spring/online/demo/OnlineApplication.java
+++ src/main/java/com/spring/online/demo/OnlineApplication.java
@@ -2,12 +2,18 @@ package com.spring.online.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OnlineApplication {
+public class OnlineApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineApplication.class, args);
 	}
 
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(OnlineApplication.class);
+	    }
 }
